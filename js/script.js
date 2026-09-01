97d94db98ab72da6fdd1a72a8ec3041f1694979e
CodeLight <codelight2023@gmail.com>
2026-09-01 13:59:34 +0100

    update

--- js/script.js
+++ js/script.js
@@ -123,19 +123,19 @@ document.addEventListener('DOMContentLoaded', () => {
 let add = document.querySelectorAll('.price .cont .add');
 let cartCount = 0;
     add.forEach((button) => {
-    button.addEventListener('click', () => {
-        button.querySelector('p').textContent = 'Added';
-        button.querySelector('i').className = 'bi bi-check-lg';
-        button.classList.add('added');
-        setTimeout(() => {
-        button.querySelector('p').textContent = 'Add';
-        button.querySelector('i').className = 'bi bi-cart';
-        button.classList.remove('added');
-    }, 3000);
-        cartCount++;
-        let show = document.querySelector('.cart .show');
-        show.style.display = 'block';
-        show.textContent = cartCount;
+        button.addEventListener('click', () => {
+            button.querySelector('p').textContent = 'Added';
+            button.querySelector('i').className = 'bi bi-check-lg';
+            button.classList.add('added');
+            setTimeout(() => {
+                button.querySelector('p').textContent = 'Add';
+                button.querySelector('i').className = 'bi bi-cart';
+                button.classList.remove('added');
+            }, 3000);
+            cartCount++;
+            let show = document.querySelector('.cart .show');
+            show.style.display = 'block';
+            show.textContent = cartCount;
         });
     });
 });
